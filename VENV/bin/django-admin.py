8c481ca545a540fd139bf35f64c774ccdf92af44
RoadPers <RoadPers@gmail.com>
2018-02-01 14:43:50 +0800

    complete post part

--- VENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/roadpers/project/pyblog/VENV/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.template.loader import get_template
 from django.http import HttpResponse
+from datetime import datetime
+
 from .models import Post
+
 # Create your views here.
 
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No.{}:".format(unicode(str(count), 'utf-8')) + unicode(str(post), 'utf-8') + "<hr>")
-        post_lists.append("<small>" + post.body + "</small><br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+   
+    try:
+        print('....................')
+        post = Post.objects.get(slug = slug)
+        
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+            return  redirect('/')
