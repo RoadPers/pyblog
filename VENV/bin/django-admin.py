8d886b02d8ce6f99854644b09e989af5f0ab13b2
RoadPers <RoadPers@gmail.com>
2018-02-01 13:00:07 +0800

    first commit with requirements.txt

---
+++ VENV/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/roadpers/project/pyblog/VENV/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ mainsite/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+# Create your views here.
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}:".format(unicode(str(count), 'utf-8')) + unicode(str(post), 'utf-8') + "<hr>")
+        post_lists.append("<small>" + post.body + "</small><br><br>")
+    return HttpResponse(post_lists)
